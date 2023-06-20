0750fbbf8e8be6d05cb55f866ec36383eedc2ec8
Joep B <joepbarmentlo@gmail.com>
2023-06-20 22:13:30 +0200

    implemented scrolling

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -76,22 +76,44 @@ void terminal_setcolor(uint8_t color)
 {
 	terminal_color = color;
 }
- 
+
+void terminal_scoll() 
+{
+	for (size_t y = 0; y < VGA_HEIGHT; y++) {
+		for (size_t x = 0; x < VGA_WIDTH; x++) {
+			const size_t index = y * VGA_WIDTH + x;
+            
+            if (y != VGA_HEIGHT - 1) {
+                terminal_buffer[index] = terminal_buffer[index + VGA_WIDTH];
+            }
+            else {
+			    terminal_buffer[index] = vga_entry(' ', terminal_color);
+            }
+		}
+	}
+    terminal_row = terminal_row - 1;
+}
+
 void terminal_putentryat(char c, uint8_t color, size_t x, size_t y) 
 {
 	const size_t index = y * VGA_WIDTH + x;
-	terminal_buffer[index] = vga_entry(c, color);
+
+    if (c == '\n') {
+		terminal_column = 0;
+        terminal_row = terminal_row + 1;
+    }
+    if (terminal_row == VGA_HEIGHT) {
+        terminal_scoll();
+    }
+    if (c != '\n') {
+	    terminal_buffer[index] = vga_entry(c, color);
+    }
 }
  
 void terminal_putchar(char c) 
 {
-    if (c == '\n') {
-		terminal_column = 0;
-        terminal_row     = terminal_row + 1;
-    }
-    else {
-	    terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
-    }
+
+    terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
 	if (++terminal_column == VGA_WIDTH) {
 		terminal_column = 0;
 		if (++terminal_row == VGA_HEIGHT)
@@ -117,5 +139,10 @@ void kernel_main(void)
  
 	/* Newline support is left as an exercise. */
 	terminal_writestring("           :::      ::::::::  \n       :+:      :+:    :+:  \n     +:+ +:+         +:+    \n   +#+  +:+       +#+       \n +#+#+#+#+#+   +#+          \n      #+#    #+#            \n     ###   ########         \n");
-
+	terminal_writestring("           :::      ::::::::  \n       :+:      :+:    :+:  \n     +:+ +:+         +:+    \n   +#+  +:+       +#+       \n +#+#+#+#+#+   +#+          \n      #+#    #+#            \n     ###   ########         \n");
+	terminal_writestring("           :::      ::::::::  \n       :+:      :+:    :+:  \n     +:+ +:+         +:+    \n   +#+  +:+       +#+       \n +#+#+#+#+#+   +#+          \n      #+#    #+#            \n     ###   ########         \n");
+	terminal_writestring("           :::      ::::::::  \n       :+:      :+:    :+:  \n     +:+ +:+         +:+    \n   +#+  +:+       +#+       \n +#+#+#+#+#+   +#+          \n      #+#    #+#            \n     ###   ########         \n");
+	terminal_writestring("           :::      ::::::::  \n       :+:      :+:    :+:  \n     +:+ +:+         +:+    \n   +#+  +:+       +#+       \n +#+#+#+#+#+   +#+          \n      #+#    #+#            \n     ###   ########         \n");
+	terminal_writestring("           :::      ::::::::  \n       :+:      :+:    :+:  \n     +:+ +:+         +:+    \n   +#+  +:+       +#+       \n +#+#+#+#+#+   +#+          \n      #+#    #+#            \n     ###   ########         \n");
 }
+// 
