c88a69fcc4dac4ff6ab0a026b9d90a82ec930963
Joep B <joep.barmentlo@echopenfactory.com>
2023-07-19 15:26:23 +0200

    colors

--- kernel/src/kernel.c
+++ kernel/src/kernel.c
@@ -61,7 +61,7 @@ void terminal_initialize(void)
 {
 	terminal_row = 0;
 	terminal_column = 0;
-	terminal_color = vga_entry_color(VGA_COLOR_LIGHT_GREY, VGA_COLOR_BLACK);
+	terminal_color = vga_entry_color(VGA_COLOR_LIGHT_BLUE, VGA_COLOR_BLACK);
 	terminal_buffer = (uint16_t*) 0xB8000;
 	for (size_t y = 0; y < VGA_HEIGHT; y++) {
 		for (size_t x = 0; x < VGA_WIDTH; x++) {
@@ -111,8 +111,10 @@ void terminal_putentryat(char c, uint8_t color, size_t x, size_t y)
  
 void terminal_putchar(char c) 
 {
-
-    terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
+	static enum vga_color color = VGA_COLOR_RED;
+	color = (color + 1) % 15 + 1;
+	// uint8_t text_color = vga_entry_color(color, VGA_COLOR_BLACK);
+    terminal_putentryat(c, vga_entry_color(color, VGA_COLOR_BLACK), terminal_column, terminal_row);
 	if (++terminal_column == VGA_WIDTH) {
 		terminal_column = 0;
 		if (++terminal_row == VGA_HEIGHT)
